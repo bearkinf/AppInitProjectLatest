3b418c6d322aaf3c01b185dd68b30b6fb75746dd
bearkinf-waem <bearkinf@waem.kr>
2023-08-24 16:47:19 +0900

    retrofit test

--- app/src/main/java/com/bearkinf/appinitprojectlatest/MainActivity.kt
+++ app/src/main/java/com/bearkinf/appinitprojectlatest/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.bearkinf.appinitprojectlatest
 
+import android.content.DialogInterface.OnClickListener
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.bearkinf.appinitprojectlatest.net.api.TestApiService
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
@@ -11,7 +13,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 class MainActivity : AppCompatActivity() {
 
 
-    val disposable: CompositeDisposable by lazy {
+    private val disposable: CompositeDisposable by lazy {
         CompositeDisposable()
     }
 
@@ -19,41 +21,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        TestApiService.service.listRepos("bearkinf")
+        TestApiService.service.listRepos("bexcvbf")
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
                 Log.e("bear", "it : ${it}")
             }, {
                 it.printStackTrace()
+                Log.e("bear", "error : ${it.message}")
+
+                errorDialog(it.message)
             })
             .let {
                 disposable.add(it)
             }
 
-        TestApiService.service2(TestApiService::class.java).listRepos("bearkinf")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.w("bear", "it : ${it}")
-            }, {
-                it.printStackTrace()
-            })
-            .let {
-                disposable.add(it)
-            }
 
-        TestApiService.service33(TestApiService::class.java).listRepos("bear")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("bear", "it : ${it}")
-            }, {
-                it.printStackTrace()
-            })
-            .let {
-                disposable.add(it)
-            }
+//        TestApiService.service2(TestApiService::class.java).listRepos("bearkinf")
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({
+//                Log.w("bear", "it : ${it}")
+//            }, {
+//                it.printStackTrace()
+//            })
+//            .let {
+//                disposable.add(it)
+//            }
+//
+//        TestApiService.service33(TestApiService::class.java).listRepos("bear")
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({
+//                Log.d("bear", "it : ${it}")
+//            }, {
+//                it.printStackTrace()
+//            })
+//            .let {
+//                disposable.add(it)
+//            }
 
     }
 
@@ -61,6 +67,22 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         disposable.dispose()
+    }
+
+
+    private fun errorDialog(msg: String?) {
+
+        AlertDialog.Builder(this@MainActivity)
+
+            .setTitle("error")
+            .setMessage(msg)
+            .setCancelable(false)
+            .setNegativeButton("확인", OnClickListener { dialog, which ->
+                dialog.dismiss()
+            })
+            .create().show()
+
+
     }
 
 
--- app/src/main/java/com/bearkinf/appinitprojectlatest/net/api/TestApiService.kt
+++ app/src/main/java/com/bearkinf/appinitprojectlatest/net/api/TestApiService.kt
@@ -13,6 +13,8 @@ import java.util.concurrent.TimeUnit
 
 interface TestApiService {
 
+    // 인터페이스 안에서 static 변수처럼 사용.
+    // 동반 객체.
     companion object {
         private val BASE_URL: String
             get() = "https://api.github.com/"
@@ -34,13 +36,12 @@ interface TestApiService {
         val service: TestApiService
             get() = retrofit.create(TestApiService::class.java)
 
-        fun <T> service2(t: Class<T>): T {
-            return retrofit.create(t)
-        }
-
-        fun <T> service33(t: Class<T>) = retrofit.create(t)
-
-
+        // 특정 인터페이스가 아닌 기능으로 구현.
+//        fun <T> service2(t: Class<T>): T {
+//            return retrofit.create(t)
+//        }
+//
+//        fun <T> service33(t: Class<T>) = retrofit.create(t)
     }
 
     @GET("users/{user}/repos")
